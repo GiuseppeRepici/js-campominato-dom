4fd71783a9b6646542e96e481a1bc3d2074728b6
GiuseppeRepici <giuseppe.repici95@gmail.com>
2022-11-10 17:01:35 +0100

    COMPLETED no extra bonus

--- js/main.js
+++ js/main.js
@@ -1,12 +1,19 @@
 const btn = document.getElementById("btn");
 let gridContainer = document.getElementById("grid-cont");
-
+let listaNumRandom = [];
+let difficolta = document.getElementById("selectdiff");
+const clickfinali = document.getElementById("clickresult");
+let numeroClick = 0;
+let celleTotVincenti= 0;
+let numeroCella;
 
 btn.addEventListener("click", function() {
+    listaNumRandom = [];
     gridContainer.innerHTML= "";
-    
-    let difficolta = document.getElementById("selectdiff");
+    difficolta = document.getElementById("selectdiff");
     console.log(difficolta.value);
+    celleTotVincenti = `${difficolta.value}`- 16 ;
+    console.log(celleTotVincenti + "vincenti");
     let numCelleRiga = Math.sqrt(`${difficolta.value}`);
     console.log(numCelleRiga);
 
@@ -15,14 +22,38 @@ btn.addEventListener("click", function() {
         const newCell = document.createElement("div");
         newCell.classList.add("cella");
         newCell.style.width = `calc(100% / ${numCelleRiga} )`;
-
         newCell.innerHTML = i+1;
         gridContainer.append(newCell);
-        newCell.addEventListener("click", function() {
+        newCell.dataset.numeroCella = i + 1;
+        
+        newCell.addEventListener("click", function logica() {
             const numeroDentro = parseInt( this.textContent );
-            newCell.classList.toggle("azzurro");
-            console.log(numeroDentro)
+            numeroCella = +this.dataset.numeroCella;
+            numeroClick++;
+            console.log(numeroClick);
+            
+            if (listaNumRandom.includes(numeroCella)){
+                numeroClick--;
+                newCell.classList.toggle("bomba");
+                clickfinali.innerHTML= `hai cliccato ${numeroClick} volte e dopo hai preso la bomba. <strong>GAME OVER!</strong>`;
+                
+            } else{
+                newCell.classList.toggle("azzurro");
+                if(`${numeroClick}`===`${celleTotVincenti}`){
+                    clickfinali.innerHTML= `hai cliccato ${numeroClick} volte senza prendere la bomba. <strong>HAI VINTO!</strong>`;
+                }
+            }
         })
     }
-
+    genListRandom();
+    console.log(listaNumRandom);
 })
+
+function genListRandom() {
+    while(listaNumRandom.length<16) {
+        numRandom = +Math.floor(Math.random()*(`${difficolta.value}`-1+1)+1);
+        if( !listaNumRandom.includes(numRandom)){
+            listaNumRandom.push(numRandom);
+        }
+    }
+}
